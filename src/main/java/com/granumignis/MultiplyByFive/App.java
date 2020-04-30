a8dc948798cd9fb7e8819aa96898cdcce361b12a
lukecool@gmail.com <lukecool@gmail.com>
2020-04-29 23:31:54 -0500

    Initial Commit

---
+++ src/main/java/com/granumignis/MultiplyByFive/App.java
@@ -0,0 +1,13 @@
+package com.granumignis.MultiplyByFive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
